e63d766a7f5ce94e124fa9eb467bd4ff2ce17c79
AU334557 <AU334557@L-185014592.wipro.com>
2018-07-24 12:17:26 +0530

    Initial commit

---
+++ TestGit/src/com/wipro/sub/Sub.java
@@ -0,0 +1,14 @@
+package com.wipro.sub;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=90;
+		int y=20;
+		int sub=x-y;
+		System.out.println(sub);
+
+	}
+
+}
---
+++ TestGit/src/com/wipro/test/Test.java
@@ -0,0 +1,14 @@
+package com.wipro.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3;
+		int b=6;
+		int sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
